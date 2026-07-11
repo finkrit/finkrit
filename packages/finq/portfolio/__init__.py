274b1b75406bce3e3ed81ff0e69077a9e1928f8d
Souvik <souvik.bagchi@gmail.com>
2026-07-11 09:54:37 -0500

    finq/portfolio: New: Add PortfolioHistory to the init in portfolio

--- packages/finq/portfolio/__init__.py
+++ packages/finq/portfolio/__init__.py
@@ -1,5 +1,6 @@
 # finkrit/packages/finq/portfolio/__init__.py
 from packages.finq.portfolio.portfolio import Portfolio
 from packages.finq.portfolio.portfoliosnapshot import PortfolioSnapshot
+from packages.finq.portfolio.portfoliohistory import PortfolioHistory
 from packages.finq.portfolio.position import Position
 from packages.finq.portfolio.positionsnapshot import PositionSnapshot
