1a3d8e8e3fd3a2e3f147fb8271cd5c5c18bf169b
Souvik <souvik.bagchi@gmail.com>
2026-07-11 12:50:10 -0500

    finq/portfolio: New: Add PortfolioData which fetches and stores asset histories

--- packages/finq/portfolio/__init__.py
+++ packages/finq/portfolio/__init__.py
@@ -1,5 +1,7 @@
 # finkrit/packages/finq/portfolio/__init__.py
 from packages.finq.portfolio.portfolio import Portfolio
+from packages.finq.portfolio.portfoliodata import PortfolioData
 from packages.finq.portfolio.portfoliosnapshot import PortfolioSnapshot
 from packages.finq.portfolio.position import Position
 from packages.finq.portfolio.positionsnapshot import PositionSnapshot
+
