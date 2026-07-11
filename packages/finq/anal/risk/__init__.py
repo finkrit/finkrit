d7ee3e7500ea4a1ce87cdfc6b0e4bdae31cf4103
Souvik <souvik.bagchi@gmail.com>
2026-07-11 17:58:42 -0500

    finq/risk: New: Add correlation to risk

--- packages/finq/anal/risk/__init__.py
+++ packages/finq/anal/risk/__init__.py
@@ -1,5 +1,7 @@
 # finkrit/packages/finq/anal/risk/__init__.py
 from packages.finq.anal.risk.beta import beta, beta_asset, beta_from_prices, beta_from_returns
-from packages.finq.anal.risk.volatility import volatility, volatility_asset, volatility_from_prices, volatility_from_returns
-from packages.finq.anal.risk.variance import variance, variance_asset, variance_from_prices, variance_from_returns
+from packages.finq.anal.risk.correlation import correlation, correlation_assets, correlation_from_prices, correlation_from_returns, correlation_matrix, correlation_matrix_from_returns
 from packages.finq.anal.risk.covariance import covariance, covariance_assets, covariance_from_prices, covariance_from_returns, covariance_matrix, covariance_matrix_from_returns
+from packages.finq.anal.risk.variance import variance, variance_asset, variance_from_prices, variance_from_returns
+from packages.finq.anal.risk.volatility import volatility, volatility_asset, volatility_from_prices, volatility_from_returns
+
