80e844d3e86b57887e33e905aef17923dfb27ac7
Souvik <souvik.bagchi@gmail.com>
2026-07-14 06:41:28 -0500

    finq/portfolio: New: Add custodian and testing

---
+++ packages/finq/datatype/custodian.py
@@ -0,0 +1,15 @@
+from enum import StrEnum
+
+
+class CustodianType(StrEnum):
+    SCHWAB = "Charles Schwab"
+    FIDELITY = "Fidelity"
+    VANGUARD = "Vanguard"
+    PERSHING = "Pershing"
+    INTERACTIVE_BROKERS = "Interactive Brokers"
+    ALTRUIST = "Altruist"
+    APEX = "Apex Clearing"
+    TRADEPMR = "TradePMR"
+    ETRADE = "E*TRADE"
+    OTHER = "Other"
+
