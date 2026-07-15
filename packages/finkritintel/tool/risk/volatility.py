1e4da497199729f6e16f525219d9e43e1ebcf6bf
Souvik <souvik.bagchi@gmail.com>
2026-07-15 12:42:07 -0500

    finkritintel/tool: New: Add volatility tool contract to risk

---
+++ packages/finkritintel/tool/risk/volatility.py
@@ -0,0 +1,7 @@
+from packages.finkritintel.tool.contract import ToolContract
+
+
+PORTFOLIO_VOLATILITY = ToolContract(
+    name="portfolio_volatility",
+    description="Compute portfolio volatility.")
+
